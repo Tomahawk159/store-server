074a5d6467d6ae67c11eb25b3028ca5e9ab01c92
Lazarev Viktor <tomahabk59@gmail.com>
2023-10-31 17:20:37 +0500

    change_FBV_on_FBV

--- store/products/views.py
+++ store/products/views.py
@@ -1,13 +1,20 @@
+from typing import Any
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.views.generic.base import TemplateView
 
 from products.models import Product, ProductCategory, Basket
-from users.models import User
 
-def index(request):
-    context = {'title': 'Store'}
-    return render(request, 'products/index.html', context)
+
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = 'Store'
+        return context
+
 
 def products(request, category_id=None, page_number=1):
     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
